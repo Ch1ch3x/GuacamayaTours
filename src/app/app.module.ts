d1226cfc2863ca5fbc46b388aa00544077e2a84e
Maria G. Cafarelli <cafarelli172006@gmail.com>
2019-10-19 10:07:52 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './components/home/home.component';
     HomeHeaderComponent,
     FooterComponent,
     SliderComponent,
+    NgImageSliderModule,
     ListHotelComponent,
     HomeComponent,
 
