e6889f8fa47fb9b30d7c64539f3d4bf7a0965dab
Maria G. Cafarelli <cafarelli172006@gmail.com>
2019-10-23 17:59:51 -0400

    estados listar and data

--- src/app/components/estados/CRUD/listar/listar-estado.component.ts
+++ src/app/components/estados/CRUD/listar/listar-estado.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTable } from '@angular/material';
+
+import estados from '../../../../data/estados.json';
+import { estado } from '../../../../interfaces/estado';
+
+const ELEMENT_DATA: estado[] = estados;
 
 @Component({
   selector: 'app-listar-estado',
   templateUrl: './listar-estado.component.html',
   styleUrls: ['./listar-estado.component.scss']
 })
-export class ListarEstadoComponent implements OnInit {
 
+export class ListarEstadoComponent implements OnInit {
+  displayedColumns: string[] = ['nombre', 'id'];
+  dataSource = ELEMENT_DATA;
+  @ViewChild(MatTable,  { static: true}) table: MatTable<any>;
+  formVisibility = false;
   constructor() { }
+  total = 2;
+  nombre = "";
+  id;
+
+  public estado = estados;
 
   ngOnInit() {
+    this.estado = estados;
+  }
+
+  openCrear() {
+    this.formVisibility = true;
+  }
+
+  crearEstado() {
+    this.formVisibility = false;
+
   }
 
 }
