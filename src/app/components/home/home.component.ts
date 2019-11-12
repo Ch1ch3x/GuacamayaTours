5e2f41992381379afee06b08c8e69e7d93a2ea68
Maria G. Cafarelli <cafarelli172006@gmail.com>
2019-11-11 22:00:11 -0400

    detalles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,25 +18,5 @@ export class HomeComponent implements OnInit {
     private fireStoreService: FirestoreService
   ) {}
   
-  imageObject: Array<object> = [
-    {
-      image: "assets/img/5011953823_19b9a06d1a_b.jpg",
-      thumbImage: "assets/img/5011953823_19b9a06d1a_b.jpg",
-      alt: "Tucacas, Estado Falcon",
-      title: "Tucacas, Estado Falcon"
-    },
-    {
-      image: "assets/img/MargaritaCastle.jpg",
-      thumbImage: "assets/img/MargaritaCastle.jpg",
-      alt: "Margarita, Estado Nueva Esparta",
-      title: "Margarita, Estado Nueva Esparta"
-    },
-    {
-      image: "assets/img/Cayo.jpg",
-      thumbImage: "assets/img/Cayo.jpg",
-      alt: "Higuerote, Estado Miranda",
-      title: "Higuerote, Estado Miranda"
-    }
-  ];
   ngOnInit() {}
 }
