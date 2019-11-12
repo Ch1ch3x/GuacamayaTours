d36764ad0b14ef8494bf300c56a3129554b196bf
Paola Ouetda <paola.outeda@corre.unimet.edu.ve>
2019-11-11 22:12:34 -0400

    removiendo el import del service

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal/public_api';
-import { AdminService } from 'src/app/services/admin.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AdminService } from "src/app/services/admin.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-  
-  constructor(
-    private modalService: BsModalService,
-    private adminService: AdminService,
-    private router: Router
-  ) { }
+  constructor(private adminService: AdminService, private router: Router) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
