9bd55e4bfde282459f7cccc7534b07d85c89dd49
Paola Ouetda <paola.outeda@corre.unimet.edu.ve>
2019-11-13 00:35:36 -0400

    filtering fields in destinos

--- src/app/components/destinos-turisticos/destinos-turisticos.component.ts
+++ src/app/components/destinos-turisticos/destinos-turisticos.component.ts
@@ -13,7 +13,10 @@ export class DestinosTuristicosComponent implements OnInit {
   private ciudades: any[] = [];
   private filteredCiudades: any[] = [];
   private categorias: any[] = [];
+  private filteredCategorias: any[] = [];
+  private destinos: any[] = [];
   estado: string;
+  ciudad: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -35,17 +38,64 @@ export class DestinosTuristicosComponent implements OnInit {
       this.filteredCiudades = this.ciudades;
     });
 
+    this.fireStoreService.getAll("destinos").subscribe(destinos => {
+      this.destinos = destinos.docs.map(destino => ({
+        ...destino.data(),
+        id: destino.id
+      }));
+    });
+
     this.fireStoreService.getAll("categorias").subscribe(categorias => {
       categorias.docs.map(categoria => {
         this.categorias.push(categoria.data());
       });
+      this.filteredCategorias = this.categorias;
     });
   }
 
-  onChange(event) {
+  onChangeEstado(event) {
     this.estado = event;
     this.filteredCiudades = this.ciudades.filter(
       ciudad => ciudad.idEstado === this.estado
+    );
+
+    this.filteredCategorias = this.destinos
+      .filter(destino => {
+        if (this.ciudad) {
+          return (
+            destino.idCiudad === this.ciudad && destino.idEstado === this.estado
+          );
+        } else return destino.idEstado === this.estado;
+      })
+      .map(filteredDestino => {
+        this.filteredCategorias = this.categorias.concat(
+          filteredDestino.categorias
+        );
+      });
+
+    this.filteredCategorias = this.categorias.filter(categoria =>
+      this.filteredCategorias.some(fc => fc === categoria.id)
+    );
+  }
+
+  onChangeCiudad(event) {
+    this.ciudad = event;
+    this.filteredCategorias = this.destinos
+      .filter(destino => {
+        if (this.estado) {
+          return (
+            destino.idCiudad === this.ciudad && destino.idEstado === this.estado
+          );
+        } else return destino.idCiudad === this.ciudad;
+      })
+      .map(filteredDestino => {
+        this.filteredCategorias = this.categorias.concat(
+          filteredDestino.categorias
+        );
+      });
+
+    this.filteredCategorias = this.categorias.filter(categoria =>
+      this.filteredCategorias.some(fc => fc === categoria.id)
     );
   }
 }
