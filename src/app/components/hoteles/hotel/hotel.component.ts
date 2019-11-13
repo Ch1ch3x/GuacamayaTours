7404490d81689fa446f97354d92f605ce8057370
Paola Outeda <paola.outeda@corre.unimet.edu.ve>
2019-11-13 18:37:18 -0400

    obteniendo el tipo de habitaciones

--- src/app/components/hoteles/hotel/hotel.component.ts
+++ src/app/components/hoteles/hotel/hotel.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router'
-import { FirestoreService } from 'src/app/services/firebase/firebase.service';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { FirestoreService } from "src/app/services/firebase/firebase.service";
 
 @Component({
-  selector: 'app-hotel',
-  templateUrl: './hotel.component.html',
-  styleUrls: ['./hotel.component.scss']
+  selector: "app-hotel",
+  templateUrl: "./hotel.component.html",
+  styleUrls: ["./hotel.component.scss"]
 })
 export class HotelComponent implements OnInit {
   private hoteles: any[] = [];
+  private tipoHabitaciones: any[] = [];
   private hotel: any;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private fireStoreService: FirestoreService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.hotel = this.route.snapshot.params['id']
-    
-    this.fireStoreService.getAll("hoteles").subscribe(hoteles => {
-      hoteles.docs.map(hotel => {
-        this.hoteles.push(hotel.data());
+    this.hotel = this.route.snapshot.params["id"];
+
+    this.fireStoreService.getDoc("hoteles", this.hotel).subscribe(hotel => {
+      console.log(hotel);
+      this.hotel = hotel.payload.data();
+      this.hotel.tipoHabitaciones.map(tipo => {
+        this.fireStoreService
+          .getAll("tipoHabitacion")
+          .subscribe(tipoHabitaciones => {
+            tipoHabitaciones.docs.map(tipoH => {
+              if (tipoH.id === tipo) {
+                this.tipoHabitaciones.push(tipoH.data());
+              }
+            });
+          });
       });
     });
   }
-
 }
