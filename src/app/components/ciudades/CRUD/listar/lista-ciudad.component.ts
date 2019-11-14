93fc11fa1d3c7446e7c85ab42590530c43bbfda2
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-14 16:16:09 -0400

    arreglando CRUD ciudades

--- src/app/components/ciudades/CRUD/listar/lista-ciudad.component.ts
+++ src/app/components/ciudades/CRUD/listar/lista-ciudad.component.ts
@@ -1,32 +1,26 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
-import { MatTable } from '@angular/material';
+import { MatTable } from "@angular/material";
 
 import ciudades from "../../../../data/ciudades.json";
 import { ciudad } from "../../../../interfaces/ciudad";
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { CiudadesService } from 'src/app/services/firebase/ciudades.service.js';
-import { EstadosService } from 'src/app/services/firebase/estados.service.js';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { CiudadesService } from "src/app/services/firebase/ciudades.service.js";
+import { EstadosService } from "src/app/services/firebase/estados.service.js";
 
 const ELEMENT_DATA: ciudad[] = ciudades;
 
 @Component({
-  selector: 'app-lista-ciudad',
-  templateUrl: './lista-ciudad.component.html',
-  styleUrls: ['./lista-ciudad.component.scss']
+  selector: "app-lista-ciudad",
+  templateUrl: "./lista-ciudad.component.html",
+  styleUrls: ["./lista-ciudad.component.scss"]
 })
 export class ListaCiudadComponent {
-
   // tslint:disable-next-line: max-line-length
-  displayedColumns: string[] = [
-    'nombre',
-    'estado',
-    'imagen',
-  ];
+  displayedColumns: string[] = ["nombre", "estado", "imagen"];
   dataSource = ELEMENT_DATA;
 
   // public ciudad = ciudades;
-
 
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
 
@@ -41,103 +35,107 @@ export class ListaCiudadComponent {
   public documentId = null;
   public currentStatus = 1;
   public newCiudadForm = new FormGroup({
-
-    nombre: new FormControl('', Validators.required),
-    idEstado: new FormControl('', Validators.required),
-    imagen: new FormControl('', Validators.required),
-    imagen2: new FormControl(''),
-    imagen3: new FormControl(''),
+    nombre: new FormControl("", Validators.required),
+    idEstado: new FormControl("", Validators.required),
+    imagen: new FormControl("", Validators.required),
+    imagen2: new FormControl(""),
+    imagen3: new FormControl(""),
     deshabilitar: new FormControl(true)
-
   });
 
-  constructor(private CiudadSV: CiudadesService, private EstadosSV: EstadosService) {
+  constructor(
+    private CiudadSV: CiudadesService,
+    private EstadosSV: EstadosService
+  ) {
     this.newCiudadForm.setValue({
-      nombre: '',
-      idEstado: '',
-      imagen: '',
-      imagen2: '',
-      imagen3: '',
-      deshabilitar: true,
+      nombre: "",
+      idEstado: "",
+      imagen: "",
+      imagen2: "",
+      imagen3: "",
+      deshabilitar: true
     });
   }
 
   ngOnInit() {
-    this.EstadosSV.getAll().subscribe((estadosSnapshot) => {
+    this.EstadosSV.getAll().subscribe(estadosSnapshot => {
       this.estados = [];
-      estadosSnapshot.forEach((estadoData: any) => {
+      estadosSnapshot.docs.forEach(estadoData => {
         this.estados.push({
-          id: estadoData.payload.doc.data(),
-          nombre: estadoData.payload.doc.data().nombre,
-          imagen: estadoData.payload.doc.data().imagen,
-          imagen2: estadoData.payload.doc.data().imagen2,
-          imagen3: estadoData.payload.doc.data().imagen3,
-          deshabilitar: estadoData.payload.doc.data().deshabilitar,
+          id: estadoData.id,
+          nombre: estadoData.data().nombre,
+          imagen: estadoData.data().imagen,
+          imagen2: estadoData.data().imagen2,
+          imagen3: estadoData.data().imagen3,
+          deshabilitar: estadoData.data().deshabilitar
         });
-      })
+      });
     });
-    this.CiudadSV.getAll().subscribe((ciudadesSnapshot) => {
+    this.CiudadSV.getAll().subscribe(ciudadesSnapshot => {
       this.ciudades = [];
-      ciudadesSnapshot.forEach((ciudadData: any) => {
+      ciudadesSnapshot.docs.forEach(ciudadData => {
         this.ciudades.push({
-          id: ciudadData.payload.doc.data(),
-          nombre: ciudadData.payload.doc.data().nombre,
-          idEstado: ciudadData.payload.doc.data().idEstado,
-          imagen: ciudadData.payload.doc.data().imagen,
-          imagen2: ciudadData.payload.doc.data().imagen2,
-          imagen3: ciudadData.payload.doc.data().imagen3,
-          deshabilitar: ciudadData.payload.doc.data().deshabilitar
+          id: ciudadData.id,
+          nombre: ciudadData.data().nombre,
+          idEstado: ciudadData.data().idEstado,
+          imagen: ciudadData.data().imagen,
+          imagen2: ciudadData.data().imagen2,
+          imagen3: ciudadData.data().imagen3,
+          deshabilitar: ciudadData.data().deshabilitar
         });
-      })
-      console.log(this.ciudades);
+      });
     });
   }
-    public newCiudad(form, documentID = this.documentId) {
-      console.log(`Status: ${this.currentStatus}`);
-      if (this.currentStatus == 1) {
-        let data = {
-          nombre: form.nombre,
-          idEstado: form.idEstado,
-          imagen: form.imagen,
-          imagen2: form.imagen2,
-          imagen3: form.imagen3,
-          deshabilitar: form.deshabilitar
-        }
-        this.CiudadSV.create(data).then(() => {
-          console.log('Documento creado exitósamente!');
+  public newCiudad(form, documentID = this.documentId) {
+    if (this.currentStatus == 1) {
+      let data = {
+        nombre: form.nombre,
+        idEstado: form.idEstado,
+        imagen: form.imagen,
+        imagen2: form.imagen2,
+        imagen3: form.imagen3,
+        deshabilitar: form.deshabilitar
+      };
+      this.CiudadSV.create(data).then(
+        () => {
+          console.log("Documento creado exitósamente!");
           this.newCiudadForm.setValue({
-            nombre: '',
-            idEstado: '',
-            imagen: '',
-            imagen2: '',
-            imagen3: '',
+            nombre: "",
+            idEstado: "",
+            imagen: "",
+            imagen2: "",
+            imagen3: "",
 
-            deshabilitar: true,
+            deshabilitar: true
           });
-        }, (error) => {
+        },
+        error => {
           console.error(error);
-        });
-      } else {
-        this.close();
-      }
+        }
+      );
+    } else {
+      this.close();
     }
+  }
 
-    public editCiudad(documentId) {
-      let editSubscribe = this.CiudadSV.getCiudad(documentId).subscribe((ciudad) => {
+  public editCiudad(documentId) {
+    let editSubscribe = this.CiudadSV.getCiudad(documentId).subscribe(
+      ciudad => {
         this.currentStatus = 2;
         this.documentId = documentId;
         this.newCiudadForm.setValue({
           id: documentId,
-          nombre: ciudad.payload.data()['nombre'],
-          idEstado: ciudad.payload.data()['estado'],
-          imagen: ciudad.payload.data()['imagen'],
-          imagen2: ciudad.payload.data()['imagen2'],
-          imagen3: ciudad.payload.data()['imagen3'],
-          deshabilitar: ciudad.payload.data()['deshabilitar'],
+          nombre: ciudad.payload.data()["nombre"],
+          idEstado: ciudad.payload.data()["estado"],
+          imagen: ciudad.payload.data()["imagen"],
+          imagen2: ciudad.payload.data()["imagen2"],
+          imagen3: ciudad.payload.data()["imagen3"],
+          deshabilitar: ciudad.payload.data()["deshabilitar"]
         });
         editSubscribe.unsubscribe();
-      });
-    }
+      }
+    );
+  }
 
   openCrear() {
     this.formVisibility = true;
@@ -173,7 +171,7 @@ export class ListaCiudadComponent {
   }
 
   soltar() {
-    this.highlight(-1)
+    this.highlight(-1);
   }
 
   deshabilitar() {
@@ -189,7 +187,7 @@ export class ListaCiudadComponent {
   habilitar() {
     for (let index = 0; index < this.ciudades.length; index++) {
       console.log(this.ciudades[index].nombre);
-      if (this.ciudades[index].id == this.selectedRowIndex){
+      if (this.ciudades[index].id == this.selectedRowIndex) {
         this.ciudades[index].deshabilitar = true;
       } else {
         continue;
