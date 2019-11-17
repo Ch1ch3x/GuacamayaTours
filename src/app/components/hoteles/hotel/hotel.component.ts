b1f622ec58dc0bc530ab4ddacd17984ea8aeefed
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-16 22:25:37 -0400

    arreglando y modificando detalle de hotel

--- src/app/components/hoteles/hotel/hotel.component.ts
+++ src/app/components/hoteles/hotel/hotel.component.ts
@@ -8,10 +8,9 @@ import { FirestoreService } from "src/app/services/firebase/firebase.service";
   styleUrls: ["./hotel.component.scss"]
 })
 export class HotelComponent implements OnInit {
-  private hoteles: any[] = [];
   private tipoHabitaciones: any[] = [];
   private hotel: any;
-  private Hotel: any;
+  private hotelId: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,14 +19,16 @@ export class HotelComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.Hotel = this.route.snapshot.params["id"];
+    this.hotelId = this.route.snapshot.params["id"];
 
-    this.fireStoreService.getAll("hoteles").subscribe(hoteles => {
-      hoteles.docs.map(hotel => {
-        this.hoteles.push(hotel.data());
-      });
+    this.fireStoreService.getDoc("hoteles", this.hotelId).subscribe(hotel => {
+      this.hotel = { ...hotel.payload.data(), id: hotel.payload.id };
+      this.fireStoreService
+        .getDoc("ciudades", this.hotel.idCiudad)
+        .subscribe((ciudad: any) => {
+          this.hotel.ciudad = ciudad.payload.data().nombre;
+        });
+      console.log(this.hotel);
     });
-    
-
   }
 }
