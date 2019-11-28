4fac1b629574fb9712aebb6c920aea62143dd9d2
freddy9824 <f.castillo@correo.unimet.edu.ve>
2019-11-28 15:31:59 -0500

    FaltaArreglar

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,6 +4,9 @@ import { AdminService } from "src/app/services/admin.service";
 import { Title } from '@angular/platform-browser';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label, SingleDataSet } from 'ng2-charts';
+import { HotelesService } from 'src/app/services/firebase/hoteles.service';
+import { TipoHabitacionService } from 'src/app/services/firebase/tipo-habitacion.service';
+import { DestinosService } from 'src/app/services/firebase/destinos.service';
 
 @Component({
   selector: "app-dashboard",
@@ -12,21 +15,70 @@ import { Label, SingleDataSet } from 'ng2-charts';
 })
 export class DashboardComponent implements OnInit {
 
+  public tipoHabitaciones = [];
+  public hoteles = [];
+  public destinos = [];
 
-
-  constructor(private adminService: AdminService, private router: Router, private titleService: Title) {}
+  constructor(private adminService: AdminService, private router: Router, private titleService: Title, private HotelSV: HotelesService, private TipoHabitacionSV: TipoHabitacionService, private DestinoSV: DestinosService ) {}
   ngOnInit() {
+    this.getData();
   }
+
+  getData() {
+    this.DestinoSV.getAll().subscribe(destinosSnapshot => {
+      this.destinos = [];
+      destinosSnapshot.docs.forEach((destinoData: any) => {
+        this.destinos.push({
+          id: destinoData.id,
+          nombre: destinoData.data().nombre,
+          descripcion: destinoData.data().descripcion,
+          categorias: destinoData.data().categorias,
+          servicios: destinoData.data().servicios,
+          actividades: destinoData
+            .data()
+            .actividades.map(actividad => actividad.nombre),
+          latitud: destinoData.data().latitud,
+          longitud: destinoData.data().longitud,
+          idEstado: destinoData.data().idEstado,
+          idCiudad: destinoData.data().idCiudad,
+          direccion: destinoData.data().direccion,
+          imagen: destinoData.data().imagen,
+          deshabilitar: destinoData.data().deshabilitar
+        });
+      });
+      console.log(this.destinos)
+    });
+    this.HotelSV.getAll().subscribe(hoteles => {
+        hoteles.docs.map(hotel=> {
+        this.hoteles.push({ ...hotel.data(), id: hotel.id });
+      });
+    });
+  }
+
   public setTitle(title){
     this.titleService.setTitle(title);
   }
+  esconder= true;
+
+  public aparecerGraficos() {
+    this.esconder = false;
+    console.log(this.esconder)
+  }
+
+  public prueba = [];
+  public valores() {
+    for (let index = 0; index < this.destinos.length; index++) {
+      this.prueba[index] = this.destinos[index].nombre;
+  } 
+}
+
 // Primer Grafico
-  public barChartOptions: ChartOptions = {
+  public barChartOptions: ChartOptions = { 
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
     scales: { xAxes: [{}], yAxes: [{}] },
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011'];
+  public barChartLabels: Label[] = this.prueba;
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
