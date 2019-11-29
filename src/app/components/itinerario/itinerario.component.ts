f7e22db1be04ee4688eddb0636137a5b69054d17
Paola Outeda <paola.outeda@correo.unimet.edu.ve>
2019-11-29 01:49:46 -0400

    agregando funciones itinerario y destino actividades

--- src/app/components/itinerario/itinerario.component.ts
+++ src/app/components/itinerario/itinerario.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FirestoreService } from "../../services/firebase/firebase.service";
 
 @Component({
-  selector: 'app-itinerario',
-  templateUrl: './itinerario.component.html',
-  styleUrls: ['./itinerario.component.scss']
+  selector: "app-itinerario",
+  templateUrl: "./itinerario.component.html",
+  styleUrls: ["./itinerario.component.scss"]
 })
 export class ItinerarioComponent implements OnInit {
+  public localizador: string;
+  public reserva: any;
+  constructor(private fireService: FirestoreService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  buscar() {
+    this.fireService.getAll("reservas").subscribe(reservas => {
+      this.reserva = reservas.docs.filter(
+        r => r.data().localizador === this.localizador
+      )[0];
+      if (this.reserva) this.reserva = this.reserva.data();
+      else alert("No existe ninguna reserva asociada a este localizador");
+    });
   }
-
 }
